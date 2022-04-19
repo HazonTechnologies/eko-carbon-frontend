34d5fcae81cfb849ea7e32d4805e0ec29b41a4b6
elpapi <charles.ikeh@thebulb.africa>
2022-04-19 09:19:17 +0100

    fixed trailingSlash

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
     },
     env: {
         NEXT_PUBLIC_SERVER_BASE_URL: 'https://jsonplaceholder.typicode.com/'
-    }
+    },
+    trailingSlash: true
 }
 
 module.exports = nextConfig
